609500c0e2f716492ec9b1d3b1b181fa68829c19
GregDigital <68654973+GregDigital@users.noreply.github.com>
2022-03-13 19:15:18 +0100

    push

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import Modal from "./lib/Modal";
+
+export default function App() {
+  const [open, setOpen] = useState(false);
+  const openModal = () => {
+    setOpen(true);
+  };
+  return (
+    <>
+      {/* isOpen allows to toggle value of open */}
+      <Modal content="Employee Created !" handleChange={setOpen} value={open} />
+      <button onClick={openModal}>button</button>
+    </>
+  );
+}
